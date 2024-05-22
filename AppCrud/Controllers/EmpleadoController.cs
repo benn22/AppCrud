0d7a405d9c51b693558a1ff48e6986dda5942ff0
benn22 <benn_2_2@hotmail.com>
2024-05-22 17:25:31 -0500

    Agregar archivos de proyecto.

---
+++ AppCrud/Controllers/EmpleadoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppCrud.Data;
+using AppCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCrud.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly AppDBContext _appDbContext;
+        public EmpleadoController(AppDBContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppCrud/Models/Empleado.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppCrud.Models
+{
+    public class Empleado
+    {        
+        public int IdEmpleado { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public DateOnly FechaContrato { get; set; }
+        public bool Activo {  get; set; }
+    }
+}
