188363618cb41fe7d70b91fa0cb8226910687b78
benn22 <benn_2_2@hotmail.com>
2024-05-25 16:57:52 -0500

    Proyecto finalizado

--- AppCrud/Controllers/EmpleadoController.cs
+++ AppCrud/Controllers/EmpleadoController.cs
@@ -13,9 +13,49 @@ namespace AppCrud.Controllers
             _appDbContext = appDbContext;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Lista()
         {
+            List<Empleado> lista = await _appDbContext.Empleados.ToListAsync();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Nuevo()
+        {            
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empleado)
+        {
+            await _appDbContext.Empleados.AddAsync(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            Empleado empleado = await _appDbContext.Empleados.FirstAsync(e => e.IdEmpleado == id);
+            return View(empleado);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Empleado empleado)
+        {
+            _appDbContext.Empleados.Update(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Empleado empleado = await _appDbContext.Empleados.FirstAsync(e => e.IdEmpleado == id);
+            _appDbContext.Empleados.Remove(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
         }
     }
 }
--- AppCrud/Program.cs
+++ AppCrud/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Empleado}/{action=Lista}/{id?}");
 
 app.Run();
